13139ad5a21130755edaf1faec44810c057c6c8b
Your Name <you@example.com>
2026-05-02 13:06:10 +0530

    fixed sum program

---
+++ Basics/sum_n.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int n,sum=0,i;
+    printf("Enter value of n:\n");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++){
+        sum += i;
+    }
+
+    printf("Sum of %d numbers = %d",n,sum);
+
+    return 0;
+}
