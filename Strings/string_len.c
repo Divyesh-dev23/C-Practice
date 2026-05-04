1e64c2f4e5c41c1d3dd91f1441b48fe2232a55c3
Your Name <you@example.com>
2026-05-04 11:19:09 +0530

    added string len code

---
+++ Strings/string_len.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int i=0;
+    char str[100];
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    while(str[i]!='\0'){
+        i++
+    }
+
+    printf("Length of the string is: %d",i);
+
+    return 0;
+}
